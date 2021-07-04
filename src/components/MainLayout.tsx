c207db6bace4e101c0d4b80c48f14b02f91e6485
Arif-hossaian <arifhossain42136@gmail.com>
2021-07-04 21:52:23 +0600

    functionality of cart done

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core";
+import { Badge, Drawer, Grid, CircularProgress } from "@material-ui/core";
 import { useQuery } from "react-query";
 import Item from "./ItemSection/Item";
 import { Wrapper, StyledButton } from "../styles/App.styles";
@@ -20,41 +20,79 @@ const getProducts = async (): Promise<CardItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const MainLayout = () => {
-  const [cartisOpen, setCartIsOpen] = useState(false);
+  const [cartOpen, setCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([] as CardItemType[]);
   const { data, isLoading, error } = useQuery<CardItemType[]>(
     "products",
     getProducts
   );
   console.log(data);
+
   const getTotalItems = (items: CardItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount, 0);
-  const handleAddToCart = (clickItem: CardItemType) => null;
-  const handleRemoveFromCart = () => null;
 
-  if (isLoading) return <LinearProgress />;
-  if (error) return <div>Somthing wrong </div>;
+  const handleAddToCart = (clickedItem: CardItemType) => {
+    setCartItems((prev) => {
+      // 1. Is the item already added in the cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) =>
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CardItemType[])
+    );
+  };
+
+  if (isLoading)
+    return (
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <CircularProgress />
+      </div>
+    );
+  if (error) return <div>Something went wrong ...</div>;
+
   return (
     <Wrapper>
-      <Drawer
-        anchor="right"
-        open={cartisOpen}
-        onClose={() => setCartIsOpen(false)}
-      >
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
         <Cart
           cartItems={cartItems}
           addToCart={handleAddToCart}
           removeFromCart={handleRemoveFromCart}
         />
       </Drawer>
-      <StyledButton onClick={() => setCartIsOpen(true)}>
+      <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
           <AddShoppingCart />
         </Badge>
       </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
-          <Grid item xs={12} sm={4} key={item.id}>
+          <Grid item key={item.id} xs={12} sm={4}>
             <Item item={item} handleAddToCart={handleAddToCart} />
           </Grid>
         ))}
--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -1,18 +1,45 @@
-import {CardItemType} from "../MainLayout"
-import { Wrapper } from "./CartItem.styles"
+import { Button } from "@material-ui/core";
+import { CardItemType } from "../MainLayout";
+import { Wrapper } from "./CartItem.styles";
 
 interface Props {
-    item: CardItemType;
-    addToCart: (clickItem: CardItemType) => void;
-    removeFromCart: (id: number) => void;
-  };
-
-const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => {
-    return (
-        <Wrapper>
-            cartitems
-        </Wrapper>
-    )
+  item: CardItemType;
+  addToCart: (clickItem: CardItemType) => void;
+  removeFromCart: (id: number) => void;
 }
 
-export default CartItem
+const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => {
+  return (
+    <Wrapper>
+      <div>
+        <h3>{item.title}</h3>
+        <div className="information">
+          <p>Price: ${item.price}</p>
+          <p>Total: ${(item.amount * item.price).toFixed(2)}</p>
+        </div>
+        <div className="buttons">
+          <Button
+            size="small"
+            variant="contained"
+            disableElevation
+            onClick={() => removeFromCart(item.id)}
+          >
+            <span style={{fontSize: "30px"}}>-</span>
+          </Button>
+          <p>{item.amount}</p>
+          <Button
+            size="small"
+            variant="contained"
+            disableElevation
+            onClick={() => addToCart(item)}
+          >
+            <span style={{fontSize: "30px"}}>+</span>
+          </Button>
+        </div>
+      </div>
+      <img src={item.image} alt={item.title} />
+    </Wrapper>
+  );
+};
+
+export default CartItem;
